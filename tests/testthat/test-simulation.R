d9df352026a0567bb21e38dfc32c60dd4f5b511b
rogoersTPH <roland.goers@swisstph.ch>
2021-11-03 15:32:18 +0100

    Fix wrong name in tests

--- tests/testthat/test-simulation.R
+++ tests/testthat/test-simulation.R
@@ -1,11 +1,11 @@
-test_that("runSimulation throws an error if cmd not found", {
+test_that("runScenarios throws an error if cmd not found", {
   expect_error(
     runScenarios(scenariosDir = "./"),
     "openMalaria could not be found"
   )
 })
 
-test_that("runSimulation throws an error if scenarios do not exist", {
+test_that("runScenarios throws an error if scenarios do not exist", {
   expect_error(
     ## R should be installed
     runScenarios(scenariosDir = "./", cmd = "R"),
