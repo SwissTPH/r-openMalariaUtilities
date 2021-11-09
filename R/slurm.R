dae5cd2cff89c2ab15c191bfa6598a045781954c
rogoersTPH <roland.goers@swisstph.ch>
2021-11-09 14:24:28 +0100

    update SLURM functions

--- R/slurm.R
+++ R/slurm.R
@@ -1,28 +1,73 @@
 ## NOTE Limit number of files (if used) to 200 per process
 
-.slurmSkeleton <- function(name, memCPU) {
+.slurmOptions <- function(jobName, ntasks = NULL, memCPU = NULL, output = NULL,
+                          error = NULL, array = NULL, time = NULL, qos = NULL) {
+  ## Header and job name
   cat(
     "#!/bin/bash
-#SBATCH --job-name=scenario_", name, "
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=", memCPU, "
-#SBATCH --output=", name_e, "scenario_%A.LOG
-#SBATCH --error=", name_e, "scenario_%A.o
-#SBATCH --array=1-", number_loops, "
-#SBATCH --time=", time, "
-#SBATCH --qos=", qos, "
-module purge
-## FIXME I am not sure if we should hardcode a R version here...
-##       But it unfucks the cluster submission for the time being.
-module load R/4.0.3-foss-2018b
-ID=$(expr ${SLURM_ARRAY_TASK_ID} - 0 )
-Rscript ", file.path(ExperimentDir, "write_scen_code.R"), " $ID",
-    sep = "", file = submit_name
+#SBATCH --job-name=", jobName, "\n",
+    ## Number of tasks per CPU
+    if (!is.null(ntasks)) {
+      paste0("#SBATCH --ntasks=", ntasks, "\n")
+    },
+    ## RAM per CPU
+    if (!is.null(memCPU)) {
+      paste0("#SBATCH --mem-per-cpu=", memCPU, "\n")
+    },
+    ## Output log file destination
+    if (!is.null(output)) {
+      paste0("#SBATCH --output=", output, "_%A_%a.log", "\n")
+    },
+    ## Error log file destination
+    if (!is.null(error)) {
+      paste0("#SBATCH --error=", error, "_%A_%a_error.log", "\n")
+    },
+    ## Job array
+    if (!is.null(array)) {
+      paste0("#SBATCH --array=1-", array, "\n")
+    },
+    ## Time limit
+    if (!is.null(time)) {
+      paste0("#SBATCH --time=", time, "\n")
+    },
+    ## Quality of service
+    if (!is.null(qos)) {
+      paste0("#SBATCH --qos=", qos, "\n")
+    },
+    sep = ""
   )
 }
 
 
-## Divide a sequence x into chunks of size n. Any rest is appended.
+writeSlurm <- function(jobName, ntasks = NULL, memCPU = NULL, output = NULL,
+                       error = NULL, array = NULL, time = NULL, qos = NULL,
+                       pre = NULL, cmd = NULL, post = NULL) {
+  cat(
+    .slurmOptions(jobName, ntasks, memCPU, output, error, array, time, qos),
+    if (!is.null(array)) {
+      paste0("ID=$(expr ${SLURM_ARRAY_TASK_ID} - 0)", "\n")
+    },
+    "\n",
+    if (!is.null(pre)) {
+      paste0(unlist(pre), "\n")
+    },
+    "\n",
+    if (!is.null(cmd)) {
+      paste0(unlist(cmd), "\n")
+    },
+    "\n",
+    if (!is.null(post)) {
+      paste0(unlist(post), "\n")
+    },
+    sep = ""
+    ## , file = "./test.sh"
+  )
+}
+
+
+##' @title Divide a sequence x into chunks of size n. Any rest is appended.
+##' @param x Sequence like 1:10
+##' @param n Chunk size
 splitSeq <- function(x, n) {
   ## Determine number of chunks
   g <- length(x) %/% n
--- tests/testthat/test-slurm.R
+++ tests/testthat/test-slurm.R
@@ -20,3 +20,53 @@ test_that("splitSeq works (with rest)", {
   )
   expect_equal(actual, expected)
 })
+
+test_that(".slurmOptions works", {
+  actual <- paste(capture.output(
+    .slurmOptions(
+      jobName = "test", ntasks = "1", memCPU = "512", output = "output",
+      error = "error", array = "400", time = "30min", qos = "30min"
+    )
+  ), sep = "", collapse = "\n")
+  expected <- "#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=512
+#SBATCH --output=output_%A_%a.log
+#SBATCH --error=error_%A_%a_error.log
+#SBATCH --array=1-400
+#SBATCH --time=30min
+#SBATCH --qos=30min"
+
+  expect_equal(actual, expected)
+})
+
+test_that("writeSlurm works", {
+  actual <- paste(capture.output(
+    writeSlurm(
+      jobName = "test", ntasks = "1", memCPU = "512", output = "output",
+      error = "error", array = "400", time = "30min", qos = "30min",
+      pre = list("module purge", "module load R/foo"),
+      cmd = list("Rscript foo.R"), post = list("echo \"Done!\"")
+    )
+  ), sep = "", collapse = "\n")
+  expected <- "#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=512
+#SBATCH --output=output_%A_%a.log
+#SBATCH --error=error_%A_%a_error.log
+#SBATCH --array=1-400
+#SBATCH --time=30min
+#SBATCH --qos=30min
+ID=$(expr ${SLURM_ARRAY_TASK_ID} - 0)
+
+module purge
+module load R/foo
+
+Rscript foo.R
+
+echo \"Done!\""
+
+  expect_equal(actual, expected)
+})
