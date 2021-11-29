e3c4394701b1b8527defebdcbc0f0c82635ef274
rogoersTPH <roland.goers@swisstph.ch>
2021-11-29 13:24:16 +0100

    Formatting

--- tests/testthat/test-xml_monitoring.R
+++ tests/testthat/test-xml_monitoring.R
@@ -1,22 +1,24 @@
-test <- monitoringContinousGen(1, list("bar" = "true", "baz" = "false"))
+actual <- monitoringContinousGen(1, list("bar" = "true", "baz" = "false"))
 expected <- list(
   period = 1, option = list("bar" = "true", "baz" = "false")
 )
 
 test_that("monitoringContinousGen works", {
-  expect_equal(test, expected)
+  expect_equal(actual, expected)
 })
 
-test <- monitoringSurveyOptionsGen(options = list("bar" = "true", "baz" = "false"))
+actual <- monitoringSurveyOptionsGen(
+  options = list("bar" = "true", "baz" = "false")
+)
 expected <- list(
   option = list("bar" = "true", "baz" = "false")
 )
 
 test_that("monitoringSurveyOptionsGen works", {
-  expect_equal(test, expected)
+  expect_equal(actual, expected)
 })
 
-test <- monitoringCohortsGen(ids = c("foo", "bar", "baz"))
+actual <- monitoringCohortsGen(ids = c("foo", "bar", "baz"))
 expected <- list(
   subPop = list(id = "foo", number = 1),
   subPop = list(id = "bar", number = 2),
@@ -24,27 +26,37 @@ expected <- list(
 )
 
 test_that("monitoringCohortsGen works", {
-  expect_equal(test, expected)
+  expect_equal(actual, expected)
 })
 
 actual <- monitoringSurveyTimesGen("2001-01-01", "2002-01-01", "1 quarter")
-expected <- list(surveyTime = list(repeatStep = "90d",
-                                   repeatEnd = as.Date("2002-12-21"),
-                                   "0d"))
+expected <- list(surveyTime = list(
+  repeatStep = "90d",
+  repeatEnd = as.Date("2002-12-21"),
+  "0d"
+))
 
 test_that("monitoringSurveyTimesGen works (regular interval)", {
   expect_equal(actual, expected)
 })
 
-actual <- monitoringSurveyTimesGen(interval = list(days = c(1, 10),
-                                         months = c(3),
-                                         years = c(2002)))
-expected <- list(surveyTime = list(repeatStep = "1y",
-                                   repeatEnd = as.Date("2003-02-28"),
-                                   "0d"),
-                 surveyTime = list(repeatStep = "1y",
-                                   repeatEnd = as.Date("2003-03-10"),
-                                   "10d"))
+actual <- monitoringSurveyTimesGen(interval = list(
+  days = c(1, 10),
+  months = c(3),
+  years = c(2002)
+))
+expected <- list(
+  surveyTime = list(
+    repeatStep = "1y",
+    repeatEnd = as.Date("2003-02-28"),
+    "0d"
+  ),
+  surveyTime = list(
+    repeatStep = "1y",
+    repeatEnd = as.Date("2003-03-10"),
+    "10d"
+  )
+)
 
 test_that("monitoringSurveyTimesGen works (block interval)", {
   expect_equal(actual, expected)
