e3efd5257f1598585b1c25071d957f66bc4021f0
rogoersTPH <roland.goers@swisstph.ch>
2022-05-03 16:09:44 +0200

    Addjust lintr settings
    Explicitly set line length and check for usage of T and F instead of
    TRUE and FALSE

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,10 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((ess-r-mode . ((flycheck-lintr-linters . "with_defaults(object_name_linter(styles = c(\"camelCase\", \"snake_case\")))")
+((ess-r-mode . ((flycheck-lintr-linters . "with_defaults(
+  line_length_linter(80),
+  nonportable_path_linter,
+  object_name_linter(styles = c(\"camelCase\", \"snake_case\")),
+  T_and_F_symbol_linter
+  )")
                 (ess-style . RStudio))))
