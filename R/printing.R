37e1d788a075e8da12f0ba9d75c1cbe00e873b43
rogoersTPH <roland.goers@swisstph.ch>
2022-05-13 12:22:17 +0200

    Add verbose and debug printing
    - Using `print()` statements within functions will simply generate
      messages which the user might disregard
    - More often then not, we are running the code non-interactively on a
      cluster
    
    The two functions `toggleVerbose` and `toggleDebug` control the output
    of `.printVerbose` and `.printDebug` which can be used within functions.
    Thus, detailed output can be made available upon request.
    This should also make future debugging easier.

---
+++ R/printing.R
@@ -0,0 +1,65 @@
+## Toggleable verbose and debug printing.
+## Instead of spamming the user with status messages, we implement toggleable
+## printing funcitons which can be used in functions. Thus, the output is
+## available only when needed.
+
+
+##' @include pkg_setup.R
+NULL
+
+
+assign("verboseOutput", FALSE, envir = .pkgenv)
+assign("debugOutput", FALSE, envir = .pkgenv)
+
+
+##' @title Toggle verbose messages
+##' @description Enables the output of verbose messages in functions which have
+##'   that implemented. Verbose output is off by default.
+##' @export
+toggleVerbose <- function() {
+  curVal <- get("verboseOutput", envir = .pkgenv)
+  if (curVal == FALSE) {
+    assign("verboseOutput", TRUE, envir = .pkgenv)
+  } else {
+    assign("verboseOutput", FALSE, envir = .pkgenv)
+  }
+}
+
+##' @title Toggle debug messages
+##' @description Enables the output of debug messages in functions which have
+##'   that implemented. Debug output is off by default.
+##' @export
+toggleDebug <- function() {
+  curVal <- get("debugOutput", envir = .pkgenv)
+  if (curVal == FALSE) {
+    assign("debugOutput", TRUE, envir = .pkgenv)
+    assign("verboseOutput", TRUE, envir = .pkgenv)
+  } else {
+    assign("debugOutput", FALSE, envir = .pkgenv)
+    assign("verboseOutput", FALSE, envir = .pkgenv)
+  }
+}
+
+##' @title Print verbose messages
+##' @description Like print, but only if 'verboseOutput' is TRUE.
+##' @param x Object to print.
+##' @param ... Further arguments to [print()].
+##' @keywords internal
+.printVerbose <- function(x, ...) {
+  curVal <- get("verboseOutput", envir = .pkgenv)
+  if (curVal == TRUE) {
+    cat(paste0("INFO\t", capture.output(print(x, ...))), sep = "\n")
+  }
+}
+
+##' @title Print debug messages
+##' @description Like print, but only if 'debugOutput' is TRUE.
+##' @param x Object to print.
+##' @param ... Further arguments to [print()].
+##' @keywords internal
+.printDebug <- function(x, ...) {
+  curVal <- get("debugOutput", envir = .pkgenv)
+  if (curVal == TRUE) {
+    cat(paste0("DEBUG\t", capture.output(print(x, ...))), sep = "\n")
+  }
+}
