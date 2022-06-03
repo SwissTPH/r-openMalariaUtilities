7b25d8eb4ea5e5f8c3064472bc5771b4d6565196
rogoersTPH <roland.goers@swisstph.ch>
2022-06-03 19:56:33 +0200

    Refactor tests for simulation.R
    - Remove any present logfiles before creating them again
    - Test for the names of the actual files instead of counting them

--- tests/testthat/test-simulation.R
+++ tests/testthat/test-simulation.R
@@ -14,67 +14,74 @@ test_that("runSimulations works", {
   )
 
   ## Normal run with console output
+  ## Make sure logfiles don't exist beforehand
+  removeFiles <- list.files(
+    file.path(getCache(x = "logsDir"), "simulation"),
+    pattern = ".*\\.log", full.names = TRUE
+  )
+  if (any(file.exists(removeFiles))) {
+    file.remove(removeFiles)
+  }
+
   expect_output(runSimulations(scenarios = scenarios, dryRun = TRUE))
 
   ## Check if log files have been written
-  ## Normal log files
-  expect_gt(
-    length(
-      list.files(
-        file.path(
-          getCache(x = "logsDir"), "simulation"
-        ),
-        pattern = ".*[0-9]+\\.log"
-      )
+  ## Expected log files
+  logfiles <- paste0("test-simulation_", c(1:5), ".log")
+  errlogfiles <- paste0("test-simulation_", c(1:5), "_error.log")
+
+
+  expect_equal(
+    list.files(
+      file.path(
+        getCache(x = "logsDir"), "simulation"
+      ),
+      pattern = ".*[0-9]+\\.log"
     ),
-    0
+    logfiles
   )
   ## Normal log files
-  expect_gt(
-    length(
-      list.files(
-        file.path(
-          getCache(x = "logsDir"), "simulation"
-        ),
-        pattern = ".*[0-9]+_error\\.log"
-      )
+  expect_equal(
+    list.files(
+      file.path(
+        getCache(x = "logsDir"), "simulation"
+      ),
+      pattern = ".*[0-9]+_error\\.log"
     ),
-    0
+    errlogfiles
   )
 
   ## Cluster run
-  file.remove(
-    list.files(
-      file.path(getCache(x = "logsDir"), "simulation"),
-      pattern = ".*\\.log"
-    )
+  ## Make sure they don't exist beforehand
+  removeFiles <- list.files(
+    file.path(getCache(x = "logsDir"), "simulation"),
+    pattern = ".*\\.log", full.names = TRUE
   )
+  if (any(file.exists(removeFiles))) {
+    file.remove(removeFiles)
+  }
 
   runSimulations(scenarios = scenarios, dryRun = TRUE, ncores = 2)
 
   ## Check if log files have been written
   ## Normal log files
-  expect_gt(
-    length(
-      list.files(
-        file.path(
-          getCache(x = "logsDir"), "simulation"
-        ),
-        pattern = ".*[0-9]+\\.log"
-      )
+  expect_equal(
+    list.files(
+      file.path(
+        getCache(x = "logsDir"), "simulation"
+      ),
+      pattern = ".*[0-9]+\\.log"
     ),
-    0
+    logfiles
   )
   ## Normal log files
-  expect_gt(
-    length(
-      list.files(
-        file.path(
-          getCache(x = "logsDir"), "simulation"
-        ),
-        pattern = ".*[0-9]+_error\\.log"
-      )
+  expect_equal(
+    list.files(
+      file.path(
+        getCache(x = "logsDir"), "simulation"
+      ),
+      pattern = ".*[0-9]+_error\\.log"
     ),
-    0
+    errlogfiles
   )
 })
