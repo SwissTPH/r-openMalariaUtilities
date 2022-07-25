622de2e0a1d310b7d5c52153a80f8d9b8c4cb78e
rogoersTPH <roland.goers@swisstph.ch>
2022-07-25 11:21:33 +0200

    Add function to validate the XML file
    - Open Malaria does validate the XML files before they are run but it
      would be better for us to do this 1) from R and 2) before Open Malaria
      is actually invoked for easier debugging and fixind
    
    - 'validateXML' uses the tools provided by 'xml2' and extracts the
      errors which are caused by the placeholders
    - We then extract the expected types and use this information to
      optionally validate the scenarios
    - The former errors are then removed from the output as they are false
      positives

---
+++ R/xml_validation.R
@@ -0,0 +1,92 @@
+## Validate created base XML file against the schema provided by OpenMalaria
+
+## We simply run 'xml_validate' from the xml2 library and exclude any error
+## which is due to the placeholder syntax.
+
+## TODO It would be much better if we could parse the XSD file and store the
+## type of the values. Then each could be validated while the XML file gets
+## created.
+
+
+##' @include pkg_setup.R
+NULL
+
+
+##' @title Validate the XML file
+##' @param xmlfile Path to XML file.
+##' @param schema Path to XSD schema file.
+##' @param scenarios Scenario data frame.
+##' @export
+validateXML <- function(xmlfile = NULL, schema = NULL, scenarios = NULL) {
+  ## If not given, get file path from cache
+  if (is.null(xmlfile)) {
+    xmlfile <- getCache(x = "baseXml")
+  }
+
+  ## If not defined, compute schema file path from cached OM version
+  if (is.null(schema)) {
+    schema <- file.path(
+      getCache("experimentDir"),
+      paste0("scenario_", getCache(x = "OMVersion"), ".xsd")
+    )
+  }
+
+  ## Check if schema and base XML file exist
+  if (!file.exists(xmlfile) || !file.exists(schema)) {
+    stop("Schema XSD file or XML file could not be found. Make sure they exist.")
+  }
+
+  ## Extract potential errors (stored as attributes)
+  errors <- attr(xml2::xml_validate(
+    xml2::read_xml(xmlfile),
+    xml2::read_xml(schema)
+  ), which = "errors")
+
+  ## Collect expected types
+  found <- unique(
+    utils::strcapture("@(.*?)@.*'xs:(.*?)'",
+      errors,
+      proto = list(key = character(), xmltype = character())
+    )
+  )
+
+  ## Add R's equivalent
+  typeDict <- data.frame(
+    xmltype = c("double", "string", "boolean", "int"),
+    rtype = c("double", "character", "logical", "integer")
+  )
+  found <- merge(found, typeDict, by = "xmltype")
+
+  ## Remove false positives (e.g. errors which are only caused by the
+  ## placeholders). Warn about remaining errors.
+  fpositives <- errors[!grepl("@(.*?)@", errors)]
+  if (length(fpositives) > 0) {
+    warning(
+      cat("Validation against schema failed:\n",
+        fpositives, "",
+        sep = "\n"
+      )
+    )
+  }
+
+  ## If scenarios are given, validate them
+  if (!is.null(scenarios)) {
+    for (i in colnames(scenarios)) {
+      actual <- typeof(scenarios[, i])
+      expected <- found[found[, "key"] == i, "rtype"]
+      if (length(expected != 0)) {
+        if (actual != expected) {
+          warning(
+            paste0(
+              i, " should be ", expected, " but was found to be ", actual, "."
+            )
+          )
+        }
+      }
+    }
+  }
+}
+
+##' @rdname validateXML
+##' @export
+validate_xml <- validateXML
