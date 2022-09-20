8fcbf2d648e22de4e68a957c75b12cd09c83df15
rogoersTPH <roland.goers@swisstph.ch>
2022-09-20 11:02:48 +0200

    Fix lintr settings
    - d748bb8 did not include all necessary changes

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,8 +3,8 @@
 
 ((ess-r-mode . ((flycheck-lintr-linters . "linters_with_defaults(
   line_length_linter(80),
-  nonportable_path_linter,
+  nonportable_path_linter(),
   object_name_linter(styles = c(\"camelCase\", \"snake_case\")),
-  T_and_F_symbol_linter
+  T_and_F_symbol_linter()
   )")
                 (ess-style . RStudio))))
